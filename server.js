30ff3b3faa127bb3de8bfacd477676426dedaac8
Artem Grygoriev <agrygoriev@gmail.com>
2018-06-22 17:57:45 +0300

    User registration added

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
+const bodyParser = require('body-parser');
 
 const users = require("./routes/api/users");
 const dossier = require("./routes/api/dossier");
 const logs = require("./routes/api/logs");
 const route = require("./routes/api/route");
+
+const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
 
 //DB config
 const db = require("./config/keys").mongoURI;
